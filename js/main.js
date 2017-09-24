59ac29ab5e79b5948bd65aaae39a7dcadebc8acb
ponchovalo <ponchovalo@outlook.com>
2017-09-24 14:47:09 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+/* If con && y ||
+var cantidadaPagar = 100;
+var saldo = 101;
+var recargaTelefono =10;
+var usuarioValido =false;
+if (saldo > cantidadaPagar && usuarioValido) {
+  console.log("Se pago correctamente");
+} else{
+  console.log ("No se puedo pagar");
+}
+
+var cantidadaPagar = 1000;
+var efectivo = 500;
+var tarjetaCredito = true;
+if (efectivo > cantidadaPagar || tarjetaCredito) {
+  console.log("Pago realizado");
+}else {
+  console.log("Te falta efectivo");
+} */
+
+/*
+var metodoDePago = 'Efectivo';
+
+switch (metodoDePago) {
+  case 'TC':
+    console.log("Se pago con tarjeta de Credito");
+    break;
+  case 'Efectivo':
+    console.log("Se pago con efectivo");
+    break;
+  case 'Paypal':
+    console.log("Se pago con Paypal");
+    break;
+  default:
+    console.log("Metodo no valido");
+    break;
+}
+*/
+//Operadores
+
+var numero1 = 10;
+var numero2 = 20;
+
+console.log(numero1+numero2);
